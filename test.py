460fcb5e945bb9a3a3a5aee27a0823eeaab83132
kevin <sharekevin001958@gmail.com>
2020-06-07 15:29:58 +0800

    gitignore

--- test.py
+++
@@ -1,9 +0,0 @@
-from configparser import ConfigParser
-
-cfg_name = input()
-cfg = ConfigParser()
-ini_path = "config/" + cfg_name + ".ini"
-cfg.read(ini_path)
-
-battle1_str = cfg['default_skill']['battle1']
-print(battle1_str)
