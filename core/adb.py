623af202b77264baf37e782ac23b5ac4b3d49f78
kevin <sharekevin001958@gmail.com>
2020-06-08 20:30:33 +0800

    import adbkit

--- core/adb.py
+++ core/adb.py
@@ -3,10 +3,10 @@ import os
 
 class adbKit(object):
     def screenshots(self):
-        os.system('adb shell screencap -p /sdcard/screencap.png')
-        os.system('adb pull /sdcard/screencap.png')
+        os.system('/adb/adb.exe shell screencap -p /sdcard/screencap.png')
+        os.system('/adb/adb.exe pull /sdcard/screencap.png')
     def click(self, pointx, pointy):
         Px = str(pointx)
         Py = str(pointy)
         print('adb shell input tap '+ Px +' '+ Py)
-        return os.system('adb shell input tap '+ Px +' '+ Py)
+        return os.system('/adb/adb.exe shell input tap '+ Px +' '+ Py)
